84b93c2d0e28a77ec8a4be211ad50c7494cecb91
Felix Yeboah <99273714+fellydyll@users.noreply.github.com>
2022-06-16 08:17:36 +0100

    Update monty_funcs_1.c

--- monty_funcs_1.c
+++ monty_funcs_1.c
@@ -1,126 +1,149 @@
+/*
+ * File: monty_funcs_1.c
+ * Auth: Bennett Dixon
+ *       Brennan D Baraban
+ */
+
 #include "monty.h"
 
-void monty_stack(stack_t **stack, unsigned int line_number);
-void monty_queue(stack_t **stack, unsigned int line_number);
-void monty_pall(stack_t **stack, unsigned int line_number);
 void monty_push(stack_t **stack, unsigned int line_number);
-
-/**
- * monty_stack - Converts a stack to a queue.
- * @stack: A pointer to the top (stack) or bottom (queue)
- *         of a stack_t linked list.
- * @line_number: The current working line number of a monty opcodes file.
- */
-void monty_stack(stack_t **stack, unsigned int line_number)
-{
-	stack_t *tmp;
-
-	if (stack == NULL || *stack == NULL)
-		return;
-
-	tmp = *stack;
-	while (tmp->prev != NULL)
-		tmp = tmp->prev;
-	stack = &tmp;
-
-	(void)line_number;
-}
-
-/**
- * monty_queue - Converts a queue to a stack.
- * @stack: A pointer to the top (stack) or bottom (queue)
- *         of a stack_t linked list.
- * @line_number: The current working line number of a monty opcodes file.
- */
-void monty_queue(stack_t **stack, unsigned int line_number)
-{
-	stack_t *tmp;
-
-	if (stack == NULL || *stack == NULL)
-		return;
-
-	tmp = *stack;
-	while (tmp->next != NULL)
-		tmp = tmp->next;
-	stack = &tmp;
-
-	(void)line_number;
-}
-
-/**
- * monty_pall - Prints a stack_t linked list.
- * @stack: A pointer to the top (stack) or bottom (queue)
- *         of a stack_t linked list.
- * @line_number: The current working line number of a monty opcodes file.
- */
-void monty_pall(stack_t **stack, unsigned int line_number)
-{
-	stack_t *tmp;
-
-	if (stack == NULL || *stack == NULL)
-		return;
-	tmp = *stack;
-
-	if (tmp->prev == NULL)
-	{
-		while (tmp)
-		{
-			printf("%d\n", tmp->n);
-			tmp = tmp->next;
-		}
-	}
-
-	else
-	{
-		while (tmp)
-		{
-			printf("%d\n", tmp->n);
-			tmp = tmp->prev;
-		}
-	}
-
-	(void)line_number;
-}
+void monty_pall(stack_t **stack, unsigned int line_number);
+void monty_pint(stack_t **stack, unsigned int line_number);
+void monty_pop(stack_t **stack, unsigned int line_number);
+void monty_swap(stack_t **stack, unsigned int line_number);
 
 /**
  * monty_push - Pushes a value to a stack_t linked list.
- * @stack: A pointer to the top (stack) or bottom (queue)
- *         of a stack_t linked list.
- * @line_number: The current working line number of a monty opcodes file.
+ * @stack: A pointer to the top mode node of a stack_t linked list.
+ * @line_number: The current working line number of a Monty bytecodes file.
  */
 void monty_push(stack_t **stack, unsigned int line_number)
 {
 	stack_t *tmp, *new;
-	int value;
+	int i;
 
 	new = malloc(sizeof(stack_t));
 	if (new == NULL)
-		malloc_error();
-
-	if (stack && *stack)
 	{
-		tmp = *stack;
-		if (tmp->next == NULL)
-		{
-			while (tmp->prev != NULL)
-				tmp = tmp->prev;
-		}
+		set_op_tok_error(malloc_error());
+		return;
 	}
-	else
-		tmp = NULL;
 
 	if (op_toks[1] == NULL)
-		no_int_error(line_number);
+	{
+		set_op_tok_error(no_int_error(line_number));
+		return;
+	}
 
-	value = atoi(op_toks[1]);
-	if (value == 0 && *op_toks[1] != '0')
-		no_int_error(line_number);
+	for (i = 0; op_toks[1][i]; i++)
+	{
+		if (op_toks[1][i] == '-' && i == 0)
+			continue;
+		if (op_toks[1][i] < '0' || op_toks[1][i] > '9')
+		{
+			set_op_tok_error(no_int_error(line_number));
+			return;
+		}
+	}
+	new->n = atoi(op_toks[1]);
 
-	new->n = value;
-	new->prev = NULL;
-	new->next = tmp;
-	if (tmp != NULL)
-		tmp->prev = new;
-	if (!stack || !(*stack) || (*stack)->next)
-		*stack = new;
+	if (check_mode(*stack) == STACK) /* STACK mode insert at front */
+	{
+		tmp = (*stack)->next;
+		new->prev = *stack;
+		new->next = tmp;
+		if (tmp)
+			tmp->prev = new;
+		(*stack)->next = new;
+	}
+	else /* QUEUE mode insert at end */
+	{
+		tmp = *stack;
+		while (tmp->next)
+			tmp = tmp->next;
+		new->prev = tmp;
+		new->next = NULL;
+		tmp->next = new;
+	}
+}
+
+/**
+ * monty_pall - Prints the values of a stack_t linked list.
+ * @stack: A pointer to the top mode node of a stack_t linked list.
+ * @line_number: The current working line number of a Monty bytecodes file.
+ */
+void monty_pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp = (*stack)->next;
+
+	while (tmp)
+	{
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
+	}
+	(void)line_number;
+}
+
+/**
+ * monty_pint - Prints the top value of a stack_t linked list.
+ * @stack: A pointer to the top mode node of a stack_t linked list.
+ * @line_number: The current working line number of a Monty bytecodes file.
+ */
+void monty_pint(stack_t **stack, unsigned int line_number)
+{
+	if ((*stack)->next == NULL)
+	{
+		set_op_tok_error(pint_error(line_number));
+		return;
+	}
+
+	printf("%d\n", (*stack)->next->n);
+}
+
+
+/**
+ * monty_pop - Removes the top value element of a stack_t linked list.
+ * @stack: A pointer to the top mode node of a stack_t linked list.
+ * @line_number: The current working line number of a Monty bytecodes file.
+ */
+void monty_pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *next = NULL;
+
+	if ((*stack)->next == NULL)
+	{
+		set_op_tok_error(pop_error(line_number));
+		return;
+	}
+
+	next = (*stack)->next->next;
+	free((*stack)->next);
+	if (next)
+		next->prev = *stack;
+	(*stack)->next = next;
+}
+
+/**
+ * monty_swap - Swaps the top two value elements of a stack_t linked list.
+ * @stack: A pointer to the top mode node of a stack_t linked list.
+ * @line_number: The current working line number of a Monty bytecodes file.
+ */
+void monty_swap(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp;
+
+	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
+	{
+		set_op_tok_error(short_stack_error(line_number, "swap"));
+		return;
+	}
+
+	tmp = (*stack)->next->next;
+	(*stack)->next->next = tmp->next;
+	(*stack)->next->prev = tmp;
+	if (tmp->next)
+		tmp->next->prev = (*stack)->next;
+	tmp->next = (*stack)->next;
+	tmp->prev = *stack;
+	(*stack)->next = tmp;
 }
